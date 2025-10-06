3f9cd55a861e2cb1f1268415af876f4137a1a1e9
Álvaro <alvaroo.ah17@gmail.com>
2025-10-06 12:29:14 +0200

    ejercicio1 acabado

---
+++ practica2/src/ejercicio1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ejercicio1 {
+
+
+    public static void main (String[] args){
+        //EjERCICIO 1
+        //Crea un array de 10 posiciones de números con valores pedidos por teclado.
+        // Muestra por consola el índice y el valor al que corresponde.
+        //----------------------------------------------------------------------------
+        Scanner sc = new Scanner(System.in);
+        int[] arrayInt = new int[10];
+        for (int i=0; i<10; i++){
+            System.out.println("Introduza un valor");
+            arrayInt[i] =sc.nextInt();
+        }
+        for (int i=0; i<10; i++){
+            System.out.println("Distintas posiciones del array[10]");
+            System.out.println(i+" --> "+ arrayInt[i]);
+        }
+    }
+
+}
