71988a94b558859873fad0b13835dca3d60a6ccc
Álvaro <alvaroo.ah17@gmail.com>
2025-10-06 13:30:22 +0200

    ejercicio2 acabado

--- practica2/src/ejercicio2.java
+++ practica2/src/ejercicio2.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class ejercicio2 {
+    public static void main(String[] args) {
+        //EjERCICIO 2
+        //Muestra por pantalla todos los elementos
+        //de un array de números enteros separados por un espacio.
+        //----------------------------------------------------------------------------
+        Scanner sc = new Scanner(System.in);
+        int[] arrayInt = new int[5];
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Introduza un valor");
+            arrayInt[i] = sc.nextInt();
+        }
+        for (int i = 0; i< arrayInt.length; i++) {
+            System.out.print(arrayInt[i]+" ");
+        }
+    }
 }
