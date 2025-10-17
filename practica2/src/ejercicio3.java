c9c28d0014c204f36f654d712506bdcbe568718e
Álvaro <alvaroo.ah17@gmail.com>
2025-10-17 08:47:47 +0200

    modificación ejercicio3 acabado

--- practica2/src/ejercicio3.java
+++ practica2/src/ejercicio3.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class ejercicio3 {
     public static void main(String[] args) {
-        //EjERCICIO 2
-        //Muestra por pantalla todos los elementos
-        //de un array de números enteros separados por un espacio.
+        //EjERCICIO 3
+        //Crea un array que contenga 5 números.
+        //Realiza un programa que te muestre por pantalla SOLO el más pequeño de ellos.
         //----------------------------------------------------------------------------
         Scanner sc = new Scanner(System.in);
         int array [] = {60,70,30,40,50};
