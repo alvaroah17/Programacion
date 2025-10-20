1b71ca7e536ff804220729461760478fb3d5150f
Álvaro <alvaroo.ah17@gmail.com>
2025-10-20 12:29:47 +0200

    ejercicio5 Terminado

--- practica2/src/ejercicio5.java
+++ practica2/src/ejercicio5.java
@@ -5,8 +5,17 @@ public class ejercicio5 {
         Scanner sc = new Scanner(System.in);
         //Crea un array de números donde
         //le indicamos por teclado el tamaño del array,
+        System.out.println("Introduce el tamaño del array por teclado");
         int n= sc.nextInt();
         double [] array=new double[n];
-        array[n]=Math.random()*9;
+        double suma=0;
+        System.out.println("Muestra el valor de cada posición");
+        for (int i=0;i< array.length;i++){
+            array[i]=Math.random()*9;
+            System.out.println(i+" "+array[i]);
+            suma+=array[i];
+        }
+        System.out.println("Suma de todos los valores aleatorios de cada posición");
+        System.out.println(suma);
     }
 }
