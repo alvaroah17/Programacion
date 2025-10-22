1dfc1747c41107ce1282ae7affb1214ec9a79934
Álvaro <alvaroo.ah17@gmail.com>
2025-10-22 13:03:58 +0200

    ejercicio6 Terminado (subo estos de golpe porque ayer no habia internet)

---
+++ practica2/src/ejercicio6.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ejercicio6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Programa Java que guarda en un array 10 números enteros que se leen por teclado. A continuación se recorre el array
+        //y calcula cuántos números son positivos, cuántos negativos y cuántos ceros.
+        System.out.println("- Introduce los 10 valores del array");
+        int [] array = new int[10];
+        //Declaramos las siguientes variables
+        int positivo=0;
+        int negativo=0;
+        int cero=0;
+        //Bucle para introducir valores en cada celda del array
+        for (int i=0;i< array.length;i++){
+            array[i]= sc.nextInt();
+            }
+        //Bucle para contar que celda del array es un numero positivo,
+        //negativo o un cero
+        for (int i=0;i< array.length;i++){
+            if (array[i]>0){
+                positivo++;
+            }
+            if (array[i]<0){
+                negativo++;
+            }
+            if (array[i]==0){
+                cero++;
+            }
+        }
+        System.out.println("Numeros positivos = "+ positivo);
+        System.out.println("Numeros negativos = "+ negativo);
+        System.out.println("Ceros = "+ cero);
+    }
+}
