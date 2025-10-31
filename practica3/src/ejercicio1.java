91671e29609290a25f5ab970739b8bf74fdaeb22
Álvaro <alvaroo.ah17@gmail.com>
2025-10-31 09:13:34 +0100

    ejercicio 1 terminado

--- practica3/src/ejercicio1.java
+++ practica3/src/ejercicio1.java
@@ -1,22 +1,26 @@
 public class ejercicio1 {
-    static void main(String[] args) {
+   public static void main(String[] args) {
         Double [][] doub = {
             {0.0, 30.0, 2.0, null, null, 5.0},
             {75.0, null, null, null, 0.0, null},
             {null, null, -2.0, 9.0, null, 11.0}
         };
+       System.out.print("Array Num|");
         for (int i = 0; i < doub.length ; i++) {
-            for (int j = 0; j < doub.length ; j++) {
-                System.out.print("Columna" +i + " ");
-                break;
-            }
-        }
-        for (int i = 0; i < doub.length; i++) {
-            System.out.print("Fila "+i+" |");
             for (int j = 0; j < doub[i].length; j++) {
-                System.out.print(doub[i][j]+" ");
+                System.out.printf("%10s","Columna "+j+"|");
+            }
+            break;
+        }
+       System.out.println();
+       System.out.println("----------------------------------------------------------------------");
+        for (int i = 0; i < doub.length; i++) {
+            System.out.printf("%10s","Fila "+i+" |");
+            for (int j = 0; j < doub[i].length; j++) {
+                System.out.printf("%10s",doub[i][j]+" |");
             }
             System.out.println();
+            System.out.println("----------------------------------------------------------------------");
         }
     }
 }
