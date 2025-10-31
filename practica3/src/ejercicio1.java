bd6f66cd72e44d754fa7e32b749b0f799b01d419
Álvaro <alvaroo.ah17@gmail.com>
2025-10-31 09:16:16 +0100

    ejercicio 1 terminado

--- practica3/src/ejercicio1.java
+++ practica3/src/ejercicio1.java
@@ -1,6 +1,10 @@
 public class ejercicio1 {
    public static void main(String[] args) {
-        Double [][] doub = {
+       /*EJERCICIO 1
+        Define un array de números tipo double de 3 filas por 7 columnas con nombre doub y asigna los valores según la siguiente tabla.
+        Muestra el contenido de todos los elementos del array dispuestos en forma de tabla como se muestra en la figura.
+        */
+       Double [][] doub = {
             {0.0, 30.0, 2.0, null, null, 5.0},
             {75.0, null, null, null, 0.0, null},
             {null, null, -2.0, 9.0, null, 11.0}
